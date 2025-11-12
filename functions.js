a4242bdd5270e8045346c824f7d936ef13d4eb30
AKA: Shortstop4313 <shortstop4313@gmail.com>
2025-11-12 14:07:49 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,10 @@
+//a wavefolder for use with a slider, sine or other -1 0 1 variable
+register('wavefold', (x, pat) => pat
+  .n(x * 2 + 3)
+  .fm(x * 4 + 3)
+)
 
+//a spinning speaker emulator for use with a slider, sine or other -1 0 1 variable
+//.whirlitzer(slider(0.5)) will get you want you want
+register('whirlitzer', (x, pat) => pat
+  .phaser((x * 3) + 4)
